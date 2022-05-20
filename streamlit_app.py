7b26f0dc46f3bcdd6d98e9e4ab10aec19dae4d45
Umutcan Kasal <96303424+UmutcanKasal@users.noreply.github.com>
2022-05-20 21:47:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
+fruit_choice = streamlit.text_input("What fruit would you like information about?")
 if not fruit_choice:
   streamlit.error("Please select a fruit to get information")
 else:
@@ -39,6 +39,8 @@ streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
   streamlit.error()
+  
+  streamlit.stop()
 
 
 
